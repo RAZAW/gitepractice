548fc87540982ab6586dc5d958e430e17bdd4baa
Waqas A. Raza <syedwaqasaliraza@hotmail.com>
2018-04-07 23:49:09 +0100

    first commit

---
+++ login_spec.rb
@@ -0,0 +1,22 @@
+#require 'selenium-webdriver'
+require_relative 'spec_helper'
+require_relative '../pages/login'
+
+describe 'Login' do
+
+  before(:each) do
+    @login = Login.new(@driver)
+  end
+
+  it 'succeeded' do
+    @login.with('tomsmith', 'SuperSecretPassword!')
+    expect(@login.success_message_present?).to eql true
+  end
+
+  it 'failed' do
+    @login.with('asdf', 'asdf')
+    expect(@login.failure_message_present?).to eql true
+    #expect(@login.success_message_present?).to eql false
+  end
+
+end
